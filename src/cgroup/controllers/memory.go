1df95ae9055834d0e6d048954b685e5a03896094
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-20 22:43:27 +0000

    memController, memLimits and swapLimits

--- src/cgroup/controllers/memory.go
+++ src/cgroup/controllers/memory.go
@@ -1,1 +1,20 @@
 package controllers
+
+type memController struct {
+	memLimits  *memLimits
+	swapLimits *swapLimits
+}
+
+type memLimits struct {
+	high int64
+	low  int64
+	max  int64
+	min  int64
+	peak int64
+}
+
+type swapLimits struct {
+	high int64
+	max  int64
+	peak int64
+}
