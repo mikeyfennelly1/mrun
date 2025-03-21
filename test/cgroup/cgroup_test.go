e8ba38d9c97def1ac6f906d695b252761b5a7402
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-21 12:43:31 +0000

    creating and destroying cgroups

--- test/cgroup/cgroup_test.go
+++ test/cgroup/cgroup_test.go
@@ -4,16 +4,59 @@ import (
 	"github.com/mikeyfennelly1/mrun/src/cgroup"
 	"github.com/stretchr/testify/require"
 	"os"
+	"syscall"
 	"testing"
 )
 
-func TestCreateCgroup(t *testing.T) {
-	// rootless testing should fail
-	if os.Geteuid() != 0 {
-		rootlessResult := cgroup.CreateCgroup("test-cgroup3")
-		require.Error(t, rootlessResult)
-	} else {
-		rootFulResult := cgroup.CreateCgroup("test-cgroup3")
-		require.NoError(t, rootFulResult)
+const (
+	RootUid        = 0
+	DefaultUid     = 1000
+	TestCgroupName = "test-cgroup3"
+)
+
+// TestDestroyCgroup
+//
+// Test destroying a control group.
+func TestDestroyCgroup(t *testing.T) {
+	result := cgroup.DestroyCgroup(TestCgroupName)
+	require.NoError(t, result)
+}
+
+// TestRootfulCgroupOperations
+//
+// Test creating a control group with rootful permissions (legal).
+func TestRootfulCgroupOperations(t *testing.T) {
+	cgroup.MustBeRoot()
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Expected no panic, but got %v", r)
+		}
+	}()
+
+	defer cgroup.DestroyCgroup(TestCgroupName)
+
+	result := cgroup.CreateCgroup(TestCgroupName)
+	defer require.NoError(t, result)
+}
+
+// TestRootLessCgroupOperationFailure
+//
+// Test attempting to perform an operation on a container without root
+// permissions (illegal, causes program panic).
+func TestRootLessCgroupOperationFailure(t *testing.T) {
+	if os.Geteuid() == RootUid {
+		syscall.Seteuid(DefaultUid)
 	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic but got none\n")
+		}
+	}()
+
+	defer cgroup.DestroyCgroup(TestCgroupName)
+
+	// this should panic
+	cgroup.CreateCgroup(TestCgroupName)
 }
