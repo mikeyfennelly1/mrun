1af26c95280796693a84b42dc905bf22f909a7c1
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-04-29 19:07:53 +0100

    remove adding pid to cgroup in CgroupInit

--- src/cgroup/cgroup.go
+++ src/cgroup/cgroup.go
@@ -5,17 +5,21 @@ import (
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-// InitCgroup creates a new control group for the container
-// and adds the containers init process to the cgroup.
-func InitCgroup(hostname string, specResources specs.LinuxResources) error {
+// InitCgroup creates a new control group for the container.
+func InitCgroup(sliceName string, specResources specs.LinuxResources) (*cgroup2.Manager, error) {
 	// get cgroup2.Resources obj from specs.LinuxResources obj
 	resources := cgroup2.ToResources(&specResources)
 
 	// create the control group as direct descendant of root user slice.
-	_, err := cgroup2.NewSystemd("/", hostname, -1, resources)
+	m, err := cgroup2.NewSystemd("/", sliceName, -1, resources)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	err = m.Update(resources)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
