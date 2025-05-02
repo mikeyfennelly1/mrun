a646c8995b92db1f43722df2da4f4adde35dcda5
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-05-02 11:34:42 +0100

    Minor cleanups to container start code.
    - Create container by containerID
    - Move pid into cgroup
    - Logging

--- cmd/start.go
+++ cmd/start.go
@@ -2,7 +2,8 @@ package cmd
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/mikeyfennelly1/mrun/src"
+	"github.com/mikeyfennelly1/mrun/src/cgroup"
 	"github.com/mikeyfennelly1/mrun/src/namespace"
 	"github.com/mikeyfennelly1/mrun/src/proc"
 	"github.com/opencontainers/runtime-spec/specs-go"
@@ -17,25 +18,39 @@ var Start = &cobra.Command{
 	Short: "Start an isolated environment.",
 	Run: func(cmd *cobra.Command, args []string) {
 		var spec specs.Spec
+
+		// read contents of ./config.json
 		jsonContent, err := os.ReadFile("./config.json")
 		if err != nil {
-			fmt.Printf("error reading config: %v", err)
+			logrus.Fatalf("error reading config: %v", err)
 			return
 		}
+
+		// unmarshal byte slice into specs.Spec structure
 		err = json.Unmarshal(jsonContent, &spec)
 		if err != nil {
-			fmt.Printf("error creating unmarshalling JSON: %v", err)
+			logrus.Fatalf("error creating unmarshalling JSON: %v", err)
 			return
 		}
 
+		// create a random containerID
+		containerID := src.CreateNewContainerID()
+		// initialize a new cgroup for the container based on the spec
+		err = cgroup.InitCgroup(containerID, spec)
+		if err != nil {
+			logrus.Fatalf("error initializing cgroup: %v", err)
+		}
+
+		cgroup.MoveCurrentPidToCgroup(containerID)
+
+		// set and apply capability sets to the process
 		proc.SetAndApplyCapsetToCurrentPid(capability.INHERITABLE, spec.Process.Capabilities.Inheritable)
-
 		proc.SetAndApplyCapsetToCurrentPid(capability.PERMITTED, spec.Process.Capabilities.Permitted)
-
 		proc.SetAndApplyCapsetToCurrentPid(capability.EFFECTIVE, spec.Process.Capabilities.Effective)
-
 		proc.SetAndApplyCapsetToCurrentPid(capability.AMBIENT, spec.Process.Capabilities.Ambient)
 
+		// execs the process with the current process program
+		// new program is running in new namespaces in chroot jail.
 		err = namespace.RestartInNewNS("chroot")
 		if err != nil {
 			logrus.Fatalf("unable to enter new namesapces")
