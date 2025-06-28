41264e3af418a9ac0df47af8912810802213c5fe
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-06-28 17:04:31 +0100

    entrypoint command 'chroot', refactored to use postChrootInitChain

--- cmd/chroot.go
+++ cmd/chroot.go
@@ -1,74 +1,45 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/mikeyfennelly1/mrun/src"
-	"github.com/opencontainers/runtime-spec/specs-go"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
-	"syscall"
 )
 
 var Chroot = &cobra.Command{
 	Use:   "chroot",
 	Short: "change the root for the binary.",
 	Run: func(cmd *cobra.Command, args []string) {
-		var spec specs.Spec
-		jsonContent, err := os.ReadFile("./config.json")
+		postChrootInitChain := getPostChrootInitChain()
+
+		spec, err := src.GetSpec()
 		if err != nil {
-			fmt.Printf("error reading config: %v", err)
-			return
-		}
-		err = json.Unmarshal(jsonContent, &spec)
-		if err != nil {
-			fmt.Printf("error creating unmarshalling JSON: %v", err)
 			return
 		}
 
-		err = syscall.Chroot("./rootfs")
-		if err != nil {
-			return
-		}
-		err = os.Chdir("./rootfs")
-		if err != nil {
-			fmt.Printf("error changing directory to rootfs: %v", err)
-			return
-		}
-
-		uid := int(spec.Process.User.UID)
-		gid := int(spec.Process.User.GID)
-		err = syscall.Setuid(uid)
-		if err != nil {
-			logrus.Warn("unable to set uid of process in container to %d\n", uid)
-		}
-		err = syscall.Setgid(gid)
-		if err != nil {
-			logrus.Warn("unable to set gid of process in container to %d\n", uid)
-		}
-
-		src.SetRLIMITsForProcess(spec.Process.Rlimits)
-
-		src.SetEnvVars(spec.Process.Env)
-
-		err = syscall.Sethostname([]byte(spec.Hostname))
-		if err != nil {
-			logrus.Warn(err)
-		}
-
-		err = src.CreateFileSystem(spec)
-		execSh()
+		postChrootInitChain.Execute(spec)
 	},
 }
 
-func execSh() {
-	shell := "/bin/sh"
-	args := []string{shell}
-	env := syscall.Environ()
+func getPostChrootInitChain() src.ChainLink {
+	chrootLink := src.ChrootLink{}
 
-	err := syscall.Exec(shell, args, env)
-	if err != nil {
-		fmt.Printf("error execing shell: %v\n", err)
-	}
+	changeProcRootLink := src.ChangeProcessDirToNewRootLink{}
+	chrootLink.SetNext(changeProcRootLink)
+
+	setUsersAndGroupsLink := src.SetUsersAndGroupsLink{}
+	changeProcRootLink.SetNext(setUsersAndGroupsLink)
+
+	setRlimitLink := src.SetRLIMITLink{}
+	setUsersAndGroupsLink.SetNext(setUsersAndGroupsLink)
+
+	setEnvVarsLink := src.SetEnvVarsLink{}
+	setRlimitLink.SetNext(setEnvVarsLink)
+
+	createFileSystemLink := src.CreateFileSystemLink{}
+	setEnvVarsLink.SetNext(createFileSystemLink)
+
+	execBinaryLink := src.ExecBinaryLink{}
+	createFileSystemLink.SetNext(execBinaryLink)
+
+	return &chrootLink
 }
