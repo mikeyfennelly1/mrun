40bc64d97d73685e8501a4d759291ca19ede97df
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-06-28 17:03:14 +0100

    ContainerMetadata singleton

---
+++ src/container_metadata.go
@@ -0,0 +1,29 @@
+package src
+
+import (
+	"sync"
+)
+
+var lock = &sync.Mutex{}
+
+type ContainerMetadata struct {
+	containerName  string
+	containerId    string
+	bundleLocation string
+}
+
+var containerMetadata *ContainerMetadata
+
+// getContainerMetadata gets the singleton ContainerMetadata
+// object and returns to the calling client.
+func getContainerMetadata() *ContainerMetadata {
+	if containerMetadata == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if containerMetadata == nil {
+			containerMetadata = &ContainerMetadata{}
+		}
+	}
+
+	return containerMetadata
+}
