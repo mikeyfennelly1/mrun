a9654efd31d2136596a4ed9df156aaa16b410b94
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-08-14 21:26:16 +0100

    chore: [init/doc.go] remove outdated docs, leaving blank for now as features in flux

--- init/doc.go
+++ init/doc.go
@@ -1,4 +1,4 @@
-// Package libinit provides an interface and a standard library of ExecutableInitStep implementations for use within larger StepChain structures.
+// Package init
 //
 // For more information see the documentation on mrun architecture: https://github.com/mikeyfennelly1/mrun/tree/main/docs
 package init
