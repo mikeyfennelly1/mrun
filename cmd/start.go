c72f21195b6822920d5492e56f22769999788188
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-08-14 21:04:25 +0100

    refactor: remove old mrun start code for interacting with Hub as Hub api has changed

--- cmd/start.go
+++ cmd/start.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"github.com/mikeyfennelly1/mrun/init"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -10,18 +8,6 @@ var Start = &cobra.Command{
 	Use:   "start",
 	Short: "Start an isolated environment.",
 	Run: func(cmd *cobra.Command, args []string) {
-		hub, err := init.Init()
-		if err != nil {
-			logrus.Fatal("could not initialize hub")
-		}
-		cur := hub.GetSteps()
-		for {
-			if cur != nil {
-				cur.Execute()
 
-				cur = *cur.Next()
-			}
-			break
-		}
 	},
 }
