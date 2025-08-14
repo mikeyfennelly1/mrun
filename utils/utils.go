b7d2e33820d69782bf8d1188f1042274256a6655
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-08-14 21:06:21 +0100

    chore: [utils.go] cleaner implementation of NewContainerID

--- utils/utils.go
+++ utils/utils.go
@@ -1,15 +1,9 @@
 package utils
 
 import (
-	"math/rand"
-	"time"
+	"github.com/google/uuid"
 )
 
 func NewContainerID() string {
-	rand.Seed(time.Now().UnixNano())
-	b := make([]byte, 16)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
+	return uuid.NewString()
 }
