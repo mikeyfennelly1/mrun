1d71d88c6e0e8b13f8903b850339e28314b8cd56
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-08-17 20:40:57 +0100

    chore: [cmd/start.go] boilerplate start command implementation with StepFactory

--- cmd/start.go
+++ cmd/start.go
@@ -1,25 +1,64 @@
 package cmd
 
 import (
+	"github.com/mikeyfennelly1/mrun/init/libinitsteps"
 	"github.com/mikeyfennelly1/mrun/utils"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var Start = &cobra.Command{
 	Use:   "start",
 	Short: "Start an isolated environment.",
 	Run: func(cmd *cobra.Command, args []string) {
-		var containerIsAlreadyBeingCreated bool = false
-		if !containerIsAlreadyBeingCreated {
-			// pre chroot steps
-			_, err := utils.GetConfigJson("./config.json")
+		if len(args) == 0 {
+			// container is not being created
+			// get config.json in pwd
+			spec, err := utils.GetConfigJson("./config.json")
 			if err != nil {
-				os.Exit(1)
+				panic(err)
+			}
+			stepNames := [...]string{
+				"init-cgroup",
+				"capset",
+				"namespace",
+			}
+
+			for _, stepName := range stepNames {
+				step, err := libinitsteps.StepFactory(stepName)
+				if err != nil {
+					panic(err)
+				}
+				err = step.Execute(spec)
+				if err != nil {
+					panic(err)
+				}
 			}
 
 		} else {
-			// post chroot steps
+			// get config.json in pwd
+			spec, err := utils.GetConfigJson("./config.json")
+			if err != nil {
+				panic(err)
+			}
+			stepNames := [...]string{
+				"chroot",
+				"usersgroups",
+				"rlimit",
+				"set-env",
+				"hostname",
+				"create-fs",
+				"exec-bin",
+			}
+			for _, stepName := range stepNames {
+				step, err := libinitsteps.StepFactory(stepName)
+				if err != nil {
+					panic(err)
+				}
+				err = step.Execute(spec)
+				if err != nil {
+					panic(err)
+				}
+			}
 		}
 	},
 }
