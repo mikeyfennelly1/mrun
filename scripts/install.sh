8f981bc8082e7862ed9022c0ec98102cff855047
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-08-26 18:47:38 +0100

    chore: Remove sudo check for install script, run commands with sudo to prompt user during running

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,8 @@
 #!/bin/zsh
 
-# Check if the script is run as root
-if [[ $(id -u) -ne 0 ]]; then
-    echo "You must be root to run this script. Use 'sudo'.." >&2
-    exit 1
-fi
-
 mrunBinaryPath="/usr/bin/mrun"
 
-go build -o ${mrunBinaryPath}
+sudo /usr/local/go/bin/go build -o ${mrunBinaryPath}
 sudo chown 0:0 ${mrunBinaryPath}
 sudo chmod +s ${mrunBinaryPath}
 
