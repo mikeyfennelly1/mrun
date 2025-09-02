61d3cb9a9ae50cdf91e0386cba292ee1bc5e0f03
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-09-02 21:43:50 +0100

    refactor: change directory before changing root with chroot

--- init/libinitsteps/proc.go
+++ init/libinitsteps/proc.go
@@ -1,11 +1,12 @@
 package libinitsteps
 
 import (
+	"os"
+	"syscall"
+
 	"github.com/mikeyfennelly1/mrun/state"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sirupsen/logrus"
-	"os"
-	"syscall"
 )
 
 // chrootStep executes the chroot syscall and moves the
@@ -13,25 +14,22 @@ import (
 type chrootStep struct{}
 
 func (c *chrootStep) Execute(spec *specs.Spec, stateManager *state.StateManager) error {
-	// perform chroot syscall
-	err := syscall.Chroot(spec.Root.Path)
-	if err != nil {
-		logrus.Errorf("an error occurred in changing root directory for this process: %v", err)
-		return err
-	}
-
 	// change cwd to the new root
 	logrus.Tracef("changing current working directory to: %s", spec.Root.Path)
-	err = os.Chdir(spec.Root.Path)
+	err := os.Chdir(spec.Root.Path)
 	if err != nil {
 		logrus.Errorf("error changing current working directory: %v", err)
 		return err
 	}
 	logrus.Tracef("changed directory to: %s", spec.Root.Path)
-	return nil
-}
 
-type setUsersAndGroupsStep struct{}
+	// perform chroot syscall
+	logrus.Infof("attempting to change fs root to: %s", spec.Root.Path)
+	err = syscall.Chroot(spec.Root.Path)
+	if err != nil {
+		logrus.Errorf("an error occurred in changing root directory to %s for this process: %v", spec.Root.Path, err)
+		return err
+	}
 
 	return nil
 }
@@ -51,6 +49,6 @@ type execBinaryStep struct{}
 func (e *execBinaryStep) Execute(spec *specs.Spec, stateManager *state.StateManager) error {
 	//TODO implement me.
 	// This should also execute shell as a default. Research if that is OCI compliant.
-	panic("implement me")
+	panic("execBinaryStep: implement me")
 	return nil
 }
