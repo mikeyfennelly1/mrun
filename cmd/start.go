60222436ee77a3dea95ca23412ce4def1b202941
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-09-02 21:42:11 +0100

    refactor: lots of `mrun start` logic moved to `mrun create`.

--- cmd/start.go
+++ cmd/start.go
@@ -1,77 +1,62 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/mikeyfennelly1/mrun/init/libinitsteps"
 	"github.com/mikeyfennelly1/mrun/state"
 	"github.com/mikeyfennelly1/mrun/utils"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
+// Start triggers step 6 of the OCI container lifecycle.
+//
+// A misconception is that the start command has no effect on the environment
+// that the application process resides in. This step in the process does have
+// an effect on what the container actually sees and does, just before it starts:
+//
+// 1. Changes the filesystem root of the environment (with the chroot syscall)
+// 2. Sets the users, and groups
+// 3. Sets the environment variables that the containerized application will run with.
+// 4. Sets the hostname of the container.
+// 5. Creates filesystem.
+// 6. Executes the application binary.
+//
+// See https://github.com/opencontainers/runtime-spec/blob/main/runtime.md#start
 var Start = &cobra.Command{
 	Use:   "start",
-	Short: "Start an isolated environment.",
+	Short: "Start a containerized application from an already created container.",
+	Args:  cobra.ExactArgs(1), // Container ID
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			// container is not being created
-			// get config.json in pwd
-			spec, err := utils.GetConfigJson("./config.json")
+		containerID := args[0]
+
+		sm := state.GetStateManager(containerID)
+		state, err := sm.FetchState()
+		if err != nil {
+			panic(err)
+		}
+
+		spec, err := utils.GetConfigJson(fmt.Sprintf("%s/config.json", state.Bundle))
+		if err != nil {
+			panic(err)
+		}
+
+		stepNames := [...]string{
+			"chroot",
+			"rlimit",
+			"set-env",
+			"hostname",
+			"create-fs",
+			"exec-bin",
+		}
+		for _, stepName := range stepNames {
+			step, err := libinitsteps.StepFactory(stepName)
 			if err != nil {
 				panic(err)
 			}
-			stepNames := [...]string{
-				"init-cgroup",
-				"capset",
-				"namespace",
-			}
-			// initialize container sm
-			containerID := utils.NewContainerID()
-			sm, err := state.NewContainerState(containerID)
-			if err != nil {
-				logrus.Fatal("unable to initialize container sm")
-				panic("exiting...")
-			}
-
-			for _, stepName := range stepNames {
-				step, err := libinitsteps.StepFactory(stepName)
-				if err != nil {
-					sm.CleanUp()
-					logrus.Fatalf("unable to get init step %s from StepFactory: %v", stepName, err)
-					os.Exit(1)
-				}
-				err = step.Execute(spec, sm)
-				if err != nil {
-					logrus.Errorf("mrun start failed with error: %v", err)
-					sm.CleanUp()
-					os.Exit(1)
-				}
-			}
-
-		} else {
-			// get config.json in pwd
-			spec, err := utils.GetConfigJson("./config.json")
+			err = step.Execute(spec, sm)
 			if err != nil {
 				panic(err)
-			}
-			stepNames := [...]string{
-				"chroot",
-				"usersgroups",
-				"rlimit",
-				"set-env",
-				"hostname",
-				"create-fs",
-				"exec-bin",
-			}
-			for _, stepName := range stepNames {
-				step, err := libinitsteps.StepFactory(stepName)
-				if err != nil {
-					panic(err)
-				}
-				err = step.Execute(spec, nil)
-				if err != nil {
-					panic(err)
-				}
 			}
 		}
 	},
